c2052a548eb51f87cb82c50d545ba7bf4a1934f6
shafwanur <shafwanur.kazi@gmail.com>
2025-07-22 23:06:22 +0200

    album-sort, global-sort, all-of implemented together with duplicate removal

--- api.py
+++ api.py
@@ -11,10 +11,12 @@ from keys import CLIENT_SECRET
 from keys import REFRESH_TOKEN
 
 class SpotifyAPI:
-    def __init__(self):
-        self.ACCESS_TOKEN = -1
+    # TODO: instrumentals, acapellas, etc. should be removed. live songs too? i don't like the current duplicate remover ...
+    def __init__(self, arg: str):
+        self.ACCESS_TOKEN = -1 # TODO: End Goal: self.ACCESS_TOKEN & self.TOKEN_TIME shall not be used like this anymore. Store them as environment variables.
         self.TOKEN_TIME = -1
         self.all_songs = []
+        self.arg = arg
 
     def refresh_token(self):
         url = 'https://accounts.spotify.com/api/token'
@@ -53,6 +55,7 @@ class SpotifyAPI:
             self.ACCESS_TOKEN = ret.json()['access_token']
             self.TOKEN_TIME = datetime.datetime.now()
 
+        # TODO: update to use a database
         with open('auth_info', 'w') as file:
             file.write(f'{self.ACCESS_TOKEN}\n{self.TOKEN_TIME}')
 
@@ -60,7 +63,7 @@ class SpotifyAPI:
 
     def process_track(self, track_id = '5VSqvL5NLxBr7uMNfjwLt8'): # feelslikeimfallinginlove from Coldplay
         '''
-        Get a single track and return its popularity, album name, track name, and URI.
+        Processes a single track and returns its popularity, album name, release date, track name, and URI.
         '''
         
         url = f'https://api.spotify.com/v1/tracks/{track_id}'
@@ -70,11 +73,11 @@ class SpotifyAPI:
         response = requests.get(url = url, headers = headers)
         if response.status_code == 200:
             response = response.json()
-            return (response['popularity'], response['album']['name'], response['name'], response['uri'])
+            return (response['popularity'], response['album']['name'], response['album']['release_date'], response['name'], response['uri'])
 
-    def process_tracks(self, album_id = '1PdMoahMiMnqWfzWZs3xSI'): # Moon Music (Full Mood Edition) from Coldplay
+    def process_album_tracks(self, album_id = '1PdMoahMiMnqWfzWZs3xSI'): # Moon Music (Full Mood Edition) from Coldplay
         '''
-        Get all tracks in an album and append them to the all_songs list.
+        Process all tracks in an album and append them to the all_songs list.
         '''
         
         url = f'https://api.spotify.com/v1/albums/{album_id}/tracks'
@@ -85,17 +88,12 @@ class SpotifyAPI:
             'limit': 50,
         }
         response = requests.get(url = url, headers = headers, params = params)
-        tmp_array = []
         for item in response.json()['items']:
-            tmp_array.append(self.process_track(track_id = item['id']))
-        
-        tmp_array.sort()
-        tmp_array = list(reversed(tmp_array))
-        self.all_songs.extend(tmp_array)
+            self.all_songs.append(self.process_track(track_id = item['id']))
     
     def process_albums(self, artist_id = '4gzpq5DPGxSnKTe4SA8HAU'): # Coldplay
         '''
-        Get all albums of an artist and process each album's tracks.
+        Process all albums of an artist and the album's tracks.
         '''
         
         url = f'https://api.spotify.com/v1/artists/{artist_id}/albums'
@@ -110,7 +108,7 @@ class SpotifyAPI:
             response = requests.get(url = url, headers = headers, params = params)
             items = response.json()['items']
             for item in items:
-                self.process_tracks(album_id = item['id'])
+                self.process_album_tracks(album_id = item['id'])
                 print(f'Processed: {item['name']}') # album processed currently
         
             url = response.json()['next']
@@ -119,24 +117,24 @@ class SpotifyAPI:
         
     def create_playlist(self, artist_name = 'Coldplay'):
         '''
-        Create a new playlist for the artist with the name "All Of: {artist_name}".
+        Create a new playlist for the artist with the name "self.arg: {artist_name}".
         '''
         
         url = f'https://api.spotify.com/v1/users/{USER_ID}/playlists'
         data = json.dumps({
-            'name': f'All Of: {artist_name}',
+            'name': " ".join(w.capitalize() for w in self.arg.split('-')) + f": {artist_name}",
             'public': True,
-            'description': f"All songs posted by {artist_name} on Spotify, sorted by popularity. Created with https://github.com/shafwanur/spotify-track-sorter"
+            'description': f"All songs posted by {artist_name} on Spotify, sorted with the {self.arg} argument. Created with https://github.com/shafwanur/spotify-track-sorter"
         })
         headers = {
             'Authorization': f'Bearer {self.access_token()}',
             'Content-Type': 'application/json'
         }
         response = requests.post(url = url, data = data, headers = headers)
-        print(f'Playlist for {artist_name} created with id {response.json()['id']}')
+        print(f'Playlist for {artist_name} created with arg {self.arg} & id {response.json()['id']}')
         return response.json()['id']
 
-    def add_to_playlist(self, playlist_id, uris):
+    def add_track_to_playlist(self, playlist_id, uris):
         '''
         Add tracks to the specified playlist with playlist_id.
         '''
@@ -166,24 +164,69 @@ class SpotifyAPI:
         response = requests.get(url = url, headers = headers)
         return response.json()['name']
 
-    def sorted_songs(self, artist_id):
-        # TODO: implement later.
-        pass
+    def sort_songs(self):
+        '''
+        Sort self.all_songs according to the argument passed. 
 
+        Possible arguments: 
+            global-sort: sort every song by popularity
+            album-sort: albums sorted by year, songs within the album sorted.
+            all-of: einfach nach der Reihenfolge der Veröffentlichung Songs in die Playlist reinhauen.
+
+        At the end, there's a layer to delete duplicate songs. Here, the song stays in the album which is the largest. 
+        '''
+        
+        # Types of Sorting available
+        # Tuple Format: (popularity, albumname, releasedate, trackname, uri)
+        if self.arg == "album-sort": 
+            self.all_songs = sorted(self.all_songs, key = lambda x: (x[2], x[1], -x[0]))
+        if self.arg == "global-sort": 
+            self.all_songs = sorted(self.all_songs, key = lambda x : -x[0])
+        if self.arg == "all-of": 
+            self.all_songs = sorted(self.all_songs, key = lambda x : (x[2], x[1]))
+
+        # Deleting Duplicates Layer
+        songs_in_album = dict()
+        for p in self.all_songs:
+            songs_in_album[p[1]] = songs_in_album.get(p[1], 0) + 1
+
+        song_album_mapping = dict()
+        for p in sorted(self.all_songs, key = lambda x: (x[3], -songs_in_album[x[1]])):
+            song_name = p[3]
+            if song_name not in song_album_mapping:
+                song_album_mapping[song_name] = p[1] # p[1] is the albumname
+
+        without_dups = [] # without the duplicates
+        for p in self.all_songs:
+            songname, albumname = p[3], p[1]
+            if song_album_mapping[songname] == albumname:
+                without_dups.append(p)
+
+        self.all_songs[:] = without_dups
+        
     def magic(self, artist_id = '4gzpq5DPGxSnKTe4SA8HAU'): # Coldplay
+        # TODO: need to update the logic here, make it übersichtlicher than just being "magic"
         '''
         Main function to process the artist's albums, create a playlist, and add all songs to it.
         '''
 
+        # Get name of the artist from artist_id
         artist_name = self.get_artist_name(artist_id)
+
+        # Create the playlist, get its id
         playlist_id = self.create_playlist(artist_name)
+
+        # Process all albums of the artist
         self.process_albums(artist_id)
-        print(f"Song Count: {len(self.all_songs)}") 
-        print(self.all_songs[:10])
 
-        print("Songs Sorted: ")
-        uris = []
+        print(f"Song Count: {len(self.all_songs)} \nSorted Songs: ") 
 
+        # Sort self.all_songs
+        self.sort_songs()
+
+        # Printing some stats
+
+        uris = [] # extracting just the uris in this step to later use to push into the newly created playlist
         file_name = os.path.join("stats", f"{artist_name}.txt")
         with open(file_name, 'a', encoding='utf-8') as file:
             for song in self.all_songs:
@@ -191,11 +234,14 @@ class SpotifyAPI:
                 print(song)
                 file.write(f"{song}\n")
         
+        # Actual pushing the songs into the newly created playlist
+        # TODO: should probably be a function on its own
         block_size = 100
         i = 0
         while i <= len(self.all_songs):
-            self.add_to_playlist(playlist_id, uris[i:i+block_size])
+            self.add_track_to_playlist(playlist_id, uris[i:i+block_size])
             i += block_size
         
+        # Success
         print(f"Success: All {len(self.all_songs)} songs added to playlist {playlist_id}")
     
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from api import SpotifyAPI
 
-api = SpotifyAPI()
+api = SpotifyAPI(arg = "all-of")
 
 print("Enter the Spotify URL of the artist:", end = " ")
-url = input()
-# url = "https://open.spotify.com/artist/5Rl15oVamLq7FbSb0NNBNy?si=PC7iNNfhS3CBrB5mVTOdiw"
+# url = input()
+url = "https://open.spotify.com/artist/6XyY86QOPPrYVGvF9ch6wz?si=Ycok5rqcTJSADDv-JwQ_Sg"
+
 artist_id = url[url.rfind("/")+1:url.find("?")]
 
-api.magic(artist_id)
+api.magic(artist_id = artist_id)
